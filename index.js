c3dbf2d0689c85bd6f7b937197d2659672af7f81
Sebastian <code@sebiann.moe>
2026-07-30 15:10:44 +0200

    Add: img, Add info for keywords
    Signed-off-by: Sebastian <code@sebiann.moe>

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ if (darkModeToggle) {
         document.body.classList.toggle('dark-mode');
     });
 }
+
+document.querySelectorAll('.keyword-row').forEach(row => {
+    row.addEventListener('click', () => {
+        const key = row.dataset.keyword;
+        const details = document.querySelector(`.keyword-details[data-parent="${key}"]`);
+        details.classList.toggle('open');
+    });
+});
